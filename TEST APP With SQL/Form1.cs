7d846df5fddf7d55aa6401a234e875c07e87943e
Dushantha Madushan <ndmwijewardhana@yahoo.com>
2012-11-20 15:54:01 +0530

    Test
    OW ye

---
+++ TEST APP With SQL/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using TEST_APP_With_SQL.util;
+
+namespace TEST_APP_With_SQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dbConnection conDB = new dbConnection();
+
+            try
+            {
+                conDB.getConnection();
+                MessageBox.Show("CONN");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ TEST APP With SQL/util/dbConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace TEST_APP_With_SQL.util
+{
+    class dbConnection
+    {
+
+        private string conString = null;
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection con = null;
+          
+            try
+            {
+                conString = "Data Source=HOME-A3B80070D9"+"\\"+"SQLEXPRESS;Initial Catalog=SWFM;Integrated Security=True";
+                con = new SqlConnection(conString);
+                con.Open();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            return con;
+        }
+    }
+}
